9edc637853c43f577e8135f6c91586e691cb76c7
Vignesh <vigneshvicky31896@gmail.com>
2025-10-16 20:42:20 +0530

    Message

--- src/main/java/com/example/WebApp.java
+++ src/main/java/com/example/WebApp.java
@@ -1,5 +1,12 @@
 package com.example;
 
 public class WebApp {
+	public static void main(String[] args) {
+		System.out.println("Hello, Jenkins CI");
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
 
 }
--- src/test/java/com/example/WebAppTest.java
+++ src/test/java/com/example/WebAppTest.java
@@ -1,5 +1,14 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class WebAppTest {
+	@Test
+	public void appsAdd() {
+		assertEquals(6, WebApp.add(3, 5));
+		
+	}
 
 }
